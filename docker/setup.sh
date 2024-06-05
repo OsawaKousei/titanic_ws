7166a3f6223a08d6b6740be6ea5a368a3ebadc7a
OsawaKousei <jwithelis@gmail.com>
2024-06-05 23:44:40 +0900

    update

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt upgrade -y
 
 # make workspace and clone repository
-cd ~
-git clone https://github.com/OsawaKousei/deep_learning_ws.git
+# cd ~
+# git clone https://github.com/OsawaKousei/titanic_ws.git
