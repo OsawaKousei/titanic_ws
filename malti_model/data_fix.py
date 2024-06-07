1ecb5264f7550c984e2eabdee2009c2a8f2ec0cc
OsawaKousei <jwithelis@gmail.com>
2024-06-07 22:37:34 +0900

    init malti model

---
+++ malti_model/data_fix.py
@@ -0,0 +1,275 @@
+import warnings
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from fancyimpute import IterativeImputer
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+
+warnings.filterwarnings("ignore")
+
+# データの読み込み
+path = "~/titanic_ws/malti_model/data/"
+train = pd.read_csv(path + "train.csv")
+test = pd.read_csv(path + "test.csv")
+
+# trainとtestを連結してdataとする
+data = pd.concat([train, test], sort=False)
+
+# 'Cabin' の先頭文字を取得
+data["Cabin"] = data["Cabin"].str[0]
+# 欠損値を 'M' で補完
+data["Cabin"].fillna("M", inplace=True)
+# 'Cabin' を数値に変換
+data["Cabin"] = data["Cabin"].map(
+    {
+        "A": 0,
+        "B": 1,
+        "C": 2,
+        "D": 3,
+        "E": 4,
+        "F": 5,
+        "G": 6,
+        "T": 7,
+        "M": 8,
+    }
+)
+
+# 'Embarked' の欠損値を最頻値で補完
+data["Embarked"].fillna(data["Embarked"].mode()[0], inplace=True)
+
+# 'Ticket'の数字でない部分を最瀕値で補完
+data["Ticket"] = data["Ticket"].str.extract("(\d+)", expand=False)
+data["Ticket"].fillna(data["Ticket"].mode()[0], inplace=True)
+
+# 'ticket' を標準化
+sc = StandardScaler()
+data["Ticket"] = sc.fit_transform(data[["Ticket"]])
+
+
+# 'Sex' と 'Embarked' のカテゴリカル特徴量を数値に変換
+data["Sex"] = data["Sex"].map({"male": 0, "female": 1})
+data["Embarked"] = data["Embarked"].map({"S": 0, "C": 1, "Q": 2})
+
+
+# 'Name' カラムから敬称を抽出し、新しい特徴量 'Title' として追加
+data["Title"] = data["Name"].str.extract(" ([A-Za-z]+)\.", expand=False)  # type: ignore
+
+# 敬称を数値に変換（頻度が低い敬称を 'Rare' としてまとめる）
+rare_titles = [
+    "Don",
+    "Rev",
+    "Dr",
+    "Mme",
+    "Ms",
+    "Major",
+    "Lady",
+    "Sir",
+    "Mlle",
+    "Col",
+    "Capt",
+    "Countess",
+    "Jonkheer",
+]
+data["Title"] = data["Title"].replace(rare_titles, "Rare")
+data["Title"] = data["Title"].map(
+    {"Mr": 0, "Miss": 1, "Mrs": 2, "Master": 3, "Rare": 4}
+)
+# 欠損値を '0' に変換
+data["Title"].fillna(0, inplace=True)
+
+# 欠損値を Embarked='S', Pclass=3 の平均値で補完
+fare = data.loc[
+    (data["Embarked"] == "S") & (data["Pclass"] == 3), "Fare"
+].median()
+data["Fare"] = data["Fare"].fillna(fare)
+
+# 'Fare'を標準化
+sc = StandardScaler()
+data["Fare_std"] = sc.fit_transform(data[["Fare"]])
+
+# 'Family_size' と 'Family_survival' を追加
+# 名前の名字を取得して'Last_name'に入れる
+data["Last_name"] = data["Name"].apply(lambda x: x.split(",")[0])
+
+data["Family_survival"] = 0.5  # デフォルトの値
+# Last_nameとFareでグルーピング
+for grp, grp_df in data.groupby(["Last_name", "Fare"]):
+
+    if len(grp_df) != 1:
+        # (名字が同じ)かつ(Fareが同じ)人が2人以上いる場合
+        for index, row in grp_df.iterrows():
+            smax = grp_df.drop(index)["Perished"].max()
+            smin = grp_df.drop(index)["Perished"].min()
+            passID = row["PassengerId"]
+
+            if smax == 1.0:
+                data.loc[data["PassengerId"] == passID, "Family_survival"] = 1
+            elif smin == 0.0:
+                data.loc[data["PassengerId"] == passID, "Family_survival"] = 0
+            # グループ内の自身以外のメンバーについて
+            # 1人でも生存している → 1
+            # 生存者がいない(NaNも含む) → 0
+            # 全員NaN → 0.5
+
+Ticket_Count = dict(data["Ticket"].value_counts())
+data["TicketGroup"] = data["Ticket"].map(Ticket_Count)
+data.loc[
+    (data["TicketGroup"] >= 2) & (data["TicketGroup"] <= 4), "Ticket_label"
+] = 2
+data.loc[
+    (data["TicketGroup"] >= 5) & (data["TicketGroup"] <= 8)
+    | (data["TicketGroup"] == 1),
+    "Ticket_label",
+] = 1
+data.loc[(data["TicketGroup"] >= 11), "Ticket_label"] = 0
+
+# Family_sizeの作成
+data["Family_size"] = data["SibSp"] + data["Parch"] + 1
+# 1, 2~4, 5~の3つに分ける
+data["Family_size_bin"] = 0
+data.loc[
+    (data["Family_size"] >= 2) & (data["Family_size"] <= 4), "Family_size_bin"
+] = 1
+data.loc[
+    (data["Family_size"] >= 5) & (data["Family_size"] <= 7), "Family_size_bin"
+] = 2
+data.loc[(data["Family_size"] >= 8), "Family_size_bin"] = 3
+
+
+# Age を Pclass, Sex, Parch, SibSp からランダムフォレストで推定
+# 推定に使用する項目を指定
+age_df = data[["Age", "Pclass", "Sex", "Parch", "SibSp"]]
+
+# ラベル特徴量をワンホットエンコーディング
+age_df = pd.get_dummies(age_df)
+
+# 学習データとテストデータに分離し、numpyに変換
+known_age = age_df[age_df.Age.notnull()].values
+unknown_age = age_df[age_df.Age.isnull()].values
+
+# 学習データをX, yに分離
+X = known_age[:, 1:]
+y = known_age[:, 0]
+
+# ランダムフォレストで推定モデルを構築
+rfr = RandomForestRegressor(random_state=0, n_estimators=100, n_jobs=-1)
+rfr.fit(X, y)
+
+# 推定モデルを使って、テストデータのAgeを予測し、補完
+predictedAges = rfr.predict(unknown_age[:, 1::])
+data.loc[(data.Age.isnull()), "Age"] = predictedAges
+
+# dead list, survie list
+
+# NameからSurname(苗字)を抽出
+data["Surname"] = data["Name"].map(lambda name: name.split(",")[0].strip())
+
+# 同じSurname(苗字)の出現頻度をカウント(出現回数が2以上なら家族)
+data["FamilyGroup"] = data["Surname"].map(data["Surname"].value_counts())
+
+
+# 家族で16才以下または女性の生存率
+Female_Child_Group = data.loc[
+    (data["FamilyGroup"] >= 2)
+    & ((data["Age"] <= 16) | (data["Sex"] == "female"))
+]
+Female_Child_Group = Female_Child_Group.groupby("Surname")["Perished"].mean()
+print("FC")
+print(Female_Child_Group)
+# 家族で16才超えかつ男性の生存率
+Male_Adult_Group = data.loc[
+    (data["FamilyGroup"] >= 2) & (data["Age"] > 16) & (data["Sex"] == "male")
+]
+Male_Adult_List = Male_Adult_Group.groupby("Surname")["Perished"].mean()
+print("MA")
+print(Male_Adult_List)
+
+# デッドリストとサバイブリストの作成
+Dead_list = set(
+    Female_Child_Group[Female_Child_Group.apply(lambda x: x == 1)].index
+)
+print(Dead_list)
+Survived_list = set(
+    Male_Adult_List[Male_Adult_List.apply(lambda x: x == 0)].index
+)
+print(Survived_list)
+
+
+# デッドリストとサバイブリストをSex, Age, Title に反映させる
+data.loc[
+    (data["Perished"].isnull())
+    & (data["Surname"].apply(lambda x: x in Dead_list)),
+    ["Sex", "Age", "Title"],
+] = [0, 28.0, 0]
+data.loc[
+    (data["Perished"].isnull())
+    & (data["Surname"].apply(lambda x: x in Survived_list)),
+    ["Sex", "Age", "Title"],
+] = [1, 5.0, 1]
+
+# ダミー変数化
+data = pd.get_dummies(
+    data=data,
+    columns=[
+        "Title",
+        "Pclass",
+        "Family_survival",
+        "Cabin",
+        "Embarked",
+        "Ticket_label",
+        "Family_size_bin",
+    ],
+)
+
+# 訓練データと本番データに再分割
+train = data.iloc[: len(train)]
+test = data.iloc[len(train) :].reset_index(drop=True)
+
+# 特徴量を選択
+features = [
+    col
+    for col in train.columns
+    if col.startswith(
+        (
+            "Pclass_",
+            "Title_",
+            "Family_survival_",
+            "Cabin_",
+            "Embarked_",
+            "Ticket_label_",
+            "Family_size_bin_",
+        )
+    )
+] + [
+    "Sex",
+    "Fare_std",
+    "Age",
+    "Parch",
+    "SibSp",
+]
+
+# 特徴量を取得
+X = train[features]
+Y = test[features]
+
+# 標準化
+scaler = StandardScaler()
+X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+Y = pd.DataFrame(scaler.transform(Y), columns=Y.columns)  # type: ignore
+
+# 特徴量をfloat型に変換
+X = X.astype("float")
+Y = Y.astype("float")
+
+# xにPerishedとPassengerIdを追加
+X["Perished"] = train["Perished"]
+X["PassengerId"] = train["PassengerId"]
+# yにPassengerIdを追加
+Y["PassengerId"] = test["PassengerId"]
+
+
+# csvファイルに保存
+X.to_csv("./malti_model/fixed_data/X.csv", index=False)
+Y.to_csv("./malti_model/fixed_data/Y.csv", index=False)
---
+++ malti_model/malti_model.py
@@ -0,0 +1,147 @@
+import os
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from filer import filter
+from mixup import mixup
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import (
+    GridSearchCV,
+    cross_validate,
+    train_test_split,
+)
+from sklearn.pipeline import make_pipeline
+from xgboost import callback
+
+warnings.filterwarnings("ignore")
+
+
+# 乱数を固定する関数
+def reset_seed(seed: int) -> None:
+    os.environ["PYTHONHASHSEED"] = "0"
+    # sklearnのシードを固定
+    np.random.seed(seed)
+
+
+# 乱数を固定
+reset_seed(622)
+
+# データの読み込み
+path = "./copy_model/fixed_data/"
+TRAIN = pd.read_csv(path + "X.csv")
+PRED = pd.read_csv(path + "Y.csv")
+prefix = pd.read_csv(path + "prefix.csv")
+
+# TRAINからPassengerIdを切り捨て
+TRAIN = TRAIN.drop("PassengerId", axis=1)
+# TRAINからPerishedを取得
+Y = TRAIN["Perished"]
+# TRAINからPerishedを削除
+X = TRAIN.drop("Perished", axis=1)
+
+# PREDからPassengerIdを取得
+PassengerId = PRED["PassengerId"]
+# PREDからPassengerIdを削除
+X_pred = PRED.drop("PassengerId", axis=1)
+
+# 訓練データとテストデータに分割
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.2, random_state=42
+)
+
+# データの形状を確認
+print("X_train_scaled.shape: ", X_train.shape)
+print("X_test_scaled.shape: ", X_test.shape)
+print("y_train.shape: ", y_train.shape)
+print("y_test.shape: ", y_test.shape)
+print("X_pred.shape: ", X_pred.shape)
+
+# ハイパーパラメータの設定
+THRESHOLD = 0.4
+LEARNING_RATE = 0.1
+MAX_DEPTH = 6
+
+# 訓練データを水増し
+# X_train, y_train = mixup(X_train, y_train, MIX_ALPHA, MIX_NUM)
+
+
+# 採用する特徴量を25個から20個に絞り込む
+select = SelectKBest(k=25)
+
+param_grid = {
+    "criterion": ["gini", "entropy"],
+    "n_estimators": [25, 100, 500, 1000, 2000],
+    "min_samples_split": [0.5, 2, 4, 10],
+    "min_samples_leaf": [1, 2, 4, 10],
+    "bootstrap": [True, False],
+}
+
+grid = GridSearchCV(
+    estimator=RandomForestClassifier(random_state=1), param_grid=param_grid
+)
+grid = grid.fit(X_train, y_train)
+
+print(grid.best_score_)
+print(grid.best_params_)
+
+# parameterをtxtファイルに保存
+filter.save_dict(grid.best_params_, "./copy_model/params/params.txt")
+
+pipeline = make_pipeline(select, grid.best_estimator_)
+pipeline.fit(X_train, y_train)
+
+
+# フィット結果の表示
+cv_result = cross_validate(pipeline, X_train, y_train, cv=10)
+print("mean_score = ", np.mean(cv_result["test_score"]))
+print("mean_std = ", np.std(cv_result["test_score"]))
+
+# --------　採用した特徴量 ---------------
+# 採用の可否状況
+mask = select.get_support()
+
+# 項目のリスト
+list_col = list(X_train.columns[1:])
+
+# 項目別の採用可否の一覧表
+for i, j in enumerate(list_col):
+    print("No" + str(i + 1), j, "=", mask[i])
+
+# シェイプの確認
+X_selected = select.transform(X)
+print("X.shape={}, X_selected.shape={}".format(X.shape, X_selected.shape))
+
+# testデータに対しての予測
+test_pred = pipeline.predict(X_test)
+# test_predをDataFrameに変換
+test_pred = pd.DataFrame(test_pred)
+# 閾値を超えたら1, そうでなければ0にする
+test_pred = (test_pred > THRESHOLD).astype(int)
+# y_testをDataFrameに変換
+y_test = pd.DataFrame(y_test)
+# 正解率を計算
+accuracy = 0.0
+for i in range(len(y_test)):
+    if y_test.iloc[i, 0] == test_pred.iloc[i, 0]:
+        accuracy += 1
+accuracy /= len(y_test)
+# 正解率を表示
+print("accuracy: ", accuracy)
+
+# 予測
+pred = pipeline.predict(X_pred)
+# predをDataFrameに変換
+pred = pd.DataFrame(pred)
+# 閾値を超えたら1, そうでなければ0にする
+pred = (pred > THRESHOLD).astype(int)
+# predにPerishedという列名をつける
+pred.columns = ["Perished"]
+# PassengerIdと結合
+submission = pd.concat([PassengerId, pred], axis=1)
+# submissionを保存
+submission.to_csv("./copy_model/predictions/submission.csv", index=False)
